813565c61298a887a08cedf8f254b62a8f8589cc
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-12-21 16:27:07 -0600

    feat: change memory to decrease the amount of characters in the request for the model

--- practice-02.py
+++ practice-02.py
@@ -1,16 +1,25 @@
 # Chatbot
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
-from dotenv import load_dotenv
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 
 load_dotenv()
 
-chat = ChatOpenAI()
+# ConversationBufferMemory is a memory used for models with chat style
+# FileChatMessageHistory is used to save the conversation in a file. It works well with ConversationBufferMemory
+# ConversationSummaryMemory is used to decrease the amount text sent to the model. It menas less money. It has its own chain
+
+chat = ChatOpenAI(verbose=True)
 
 # The return_messages param stores the strings given by the user and the AI using the HumanPromptTemplate and AIPropmtTemplate
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationSummaryMemory(
+  memory_key="messages", 
+  return_messages=True, 
+  llm=chat
+  # chat_memory=FileChatMessageHistory("messages_history.json")
+)
 
 prompt = ChatPromptTemplate(
   input_variables=["content", "messages"],
@@ -20,7 +29,7 @@ prompt = ChatPromptTemplate(
   ]
 )
 
-chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory, verbose=True)
 
 while True:
   content = input(">> ")
